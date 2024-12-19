6da0e28b5540f2fa79873b454cd74eab9a360b60
gabis <gabisovmaxim@gmail.com>
2024-12-19 18:10:04 +0300

    Removed unused files and created Unload function.

--- Source/Scripts/MainMenu/MainMenu.py
+++ Source/Scripts/MainMenu/MainMenu.py
@@ -7,16 +7,7 @@ class MainMenu:
         self.screen = pygame.display.set_mode((1280, 720))
         pygame.display.set_caption('Главное Меню')
         self.levels = []
-        self.LoadMap()
         self.LoadLevels()
-
-    def LoadMap(self):
-        self.screen.fill((255, 255, 255))
-        self.map = pygame.image.load("../Images/Map.png")
-        rules.append(self.RenderMap) # Пример добавления правила
-
-    def RenderMap(self):
-        self.screen.blit(self.map, (0, 0))
 
     def LoadLevels(self):
         pass
